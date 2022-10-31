b1743c3008e269a23a97e91527f3bd55964a86c1
CucomberHacker31 <112803310+CucomberHacker31@users.noreply.github.com>
2022-10-31 09:35:17 +0300

    Start

---
+++ ex.js
@@ -0,0 +1,17 @@
+function square(side) {
+    for(var i = 0; i < 4; i++){
+       forward(side);
+       right(90);
+    }
+ }
+ function main()
+{
+    pendown();
+    var min = -getWidth()/2;
+    var max = getWidth()/2;
+    goto(-250,250);
+    console.log(tutle.pos)
+    Left(45)
+    forward(705)
+    
+}
